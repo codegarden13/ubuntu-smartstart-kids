6213498002d2064d6eeb4c4e91b855e642725698
codegarden13 <code@xviz.de>
2025-12-15 12:52:02 +0100

    Fixed edge setup

---
+++ install/helpers/install-MicrosoftEdge.sh
@@ -0,0 +1,7 @@
+
+# ==========================================
+# MS Edge für Ubuntu 
+# ==========================================
+cd /tmp
+curl -LO https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_current_amd64.deb
+sudo apt install ./microsoft-edge-stable_current_amd64.deb
